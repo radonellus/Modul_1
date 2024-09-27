f20ed23c0e9ead6d89c9446df3d1761bbe255c92
radonellus <radonellus@gmail.com>
2024-09-27 23:06:33 +0300

    Create module_1_4.py

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Введите текст: ") #команда для ввода значения
+print(len(my_string)) #вывод значения в количестве символов
+print(my_string .upper()) #вывод строки my_string в верхнем регистре
+print(my_string .lower()) #вывод строки my_string в нижнем регистре
+print(my_string .replace(' ','')) #вывод строки my_string удалив все пробелы
+print(my_string[0]) #вывод первого символа строки my_string
+print(my_string[-1]) #вывод последнего символа my_string
